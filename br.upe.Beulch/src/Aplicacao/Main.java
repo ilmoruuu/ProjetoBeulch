972ed4fe9fd27ce5b5c8f0353a3f0aa873b6340f
Murilo Lucena <muriloglucenacanal@gmail.com>
2024-10-14 22:34:26 -0300

    correção relacionadas à problemáticas de conflitos de merges

--- br.upe.Beulch/src/Aplicacao/Main.java
+++ br.upe.Beulch/src/Aplicacao/Main.java
@@ -4,26 +4,6 @@ import PadroesProjeto.FactoryMethod.*;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("João", "joao@email.com", "123");
-        Cliente cliente2 = new Cliente("Pedro", "pedro@email.com", "456");
-
-        Produto produto1 = new Cafe("café", 3f);
-
-
-        Factory cafeFactory= new FactoryCafe();
-        Produto cafe = cafeFactory.criarProduto("Espresso", 22);
-
-        Factory livroFactory = new FactoryLivro();
-        Produto livro = livroFactory.criarProduto("Padroes de Projeto", 35678);
-
-        Factory boloFactory = new FactoryBolo();
-        Produto bolo = boloFactory.criarProduto("chocolate", 567);
-
-        produto1.adicionarObservador(cliente1);
-        produto1.setDisponibilidade(true);
-        produto1.setPrecoProduto(30f);
-        produto1.setDisponibilidade(false);
-        produto1.setDisponibilidade(true);
-
     }
+
 }
