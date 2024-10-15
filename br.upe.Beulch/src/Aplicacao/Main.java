c789472e211f18e976ef52af939edbb5a138a514
laahnanes <larahnanes160@gmail.com>
2024-10-15 10:13:21 -0300

    modificações para dar merge

--- br.upe.Beulch/src/Aplicacao/Main.java
+++ br.upe.Beulch/src/Aplicacao/Main.java
@@ -4,13 +4,5 @@ import PadroesProjeto.FactoryMethod.Cafe;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("João","joao@gmail.com", "123");
-
-        Produto cafe = new Cafe("Expresso", 3f);
-
-        cafe.adicionarObservador(cliente);
-        cafe.setPrecoProduto(5f);
-        cafe.setDisponibilidade(false);
-
     }
 }
